85821edc5d0d9f721328cf216071193fea9d19c6
jorunn <jorjo335@student.liu.se>
2023-12-03 15:13:48 +0100

    day2 task 1 complete

--- day2.py
+++ day2.py
@@ -3,9 +3,27 @@ def readFile(file):
     f = open(file)
     return f.readlines()
 
-def isPossible(line): 
+
+def isPossible(line):
     lineList = line.split("; ")
-    print(lineList)
+    #print(lineList)
+    for draw in lineList:
+        #print(draw)
+        color = draw.split(", ")
+        for i in color:
+            number = i.split(" ")
+            #print(number[0])
+            if "red" in number: 
+                if int(number[0]) >12: 
+                    return False
+            if "blue" in number: 
+                if int(number[0]) > 14: 
+                    return False
+            if "green" in number: 
+                if int(number[0]) > 13: 
+                    return False
+    return True
+
 
 
 def main(): 
@@ -13,10 +31,12 @@ def main():
     index = 1
     result = 0
     for line in file:
+        x = line.split(": ")
+        line = x[1]
         if isPossible(line):
             result += index
         index += 1
-
+    print(result)
 
 main()
 
