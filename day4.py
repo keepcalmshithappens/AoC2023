bbbaeac432970407c86ae3518bae5121ec4c2f56
jorunn <jorjo335@student.liu.se>
2023-12-04 20:15:25 +0100

    day 4 pt 1

--- day4.py
+++ day4.py
@@ -1,24 +1,28 @@
-   
 
 def main(): 
     lines = open("input4.txt").readlines()
+    finalResult = 0
     for line in lines: 
         line = line.split(": ")[1]
         winner = line.split("| ")[0]
         mine = line.split("| ")[1]
         mine = mine.split(" ")
         winner = winner.split(" ")
-        finalResult = 0
+        result = 0
         for i in winner:
+            if i == '':
+                continue
             index = 0
-            result = 0
             for nr in mine:
+                if index == len(mine)-1: 
+                    nr = nr[:-1]
                 if nr == i:
-                    print(i, nr)
                     result += 1
-                    mine.remove(nr)
-        finalResult += 2**result 
-    #print(finalResult)
+                    #mine.remove(mine[index])
+                index += 1
+            print("result", result)
+        if result: 
+            finalResult += 2**(result-1) 
+        print("finalresult ", finalResult)
+
 main()
-
-
